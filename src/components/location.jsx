bfee3686e0e63407c4547cfc49eda1b302401b39
KWANGHO UM <kwanghoum@KWANGHOs-MacBook-Pro.local>
2025-05-19 21:24:37 +0900

    kakao map

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -47,8 +47,8 @@ const Location = () => {
   const executeScript = () => {
     const scriptTag = document.createElement("script");
     const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
-    "timestamp" : "1743289799108",
-    "key" : "2ni72",
+    "timestamp" : "1747657394716",
+    "key" : "2o3ni",
     "mapWidth" : "640",
     "mapHeight" : "360"
   }).render();`);
