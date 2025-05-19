ba889740783cbaee9c2ff826571cd68dcb548d50
KWANGHO UM <kwanghoum@KWANGHOs-MacBook-Pro.local>
2025-05-19 23:57:03 +0900

    quote font size

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -20,7 +20,7 @@ const Content = styled.span`
   font-family: "mom_to_daughter";
   text-align: center;
   color: var(--title-color);
-  line-height: 3.4rem;
+  line-height: 3.6rem;
   opacity: 0.75;
   background-image: url(${QuotePaper});
   background-repeat: no-repeat;
